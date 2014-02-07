a1be2a396281c40bf0143761e2dde6afa515b42d
jpardobl <jpardo@digitalhigh.es>
2014-02-07 13:49:51 +0100

    Worked out the host and host_datasources resources.

---
+++ cacti_rest/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('',
+    url(r'^host/(\d+)/data_sources', 'cacti_rest.resource_host_datasource.entrance', name='resource_host_datasource'),
+    url(r'^host', 'cacti_rest.resource_host.entrance', name='resource_host'),    
+)
